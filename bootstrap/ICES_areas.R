6e369dee2027ed394ed37ddc0b16f1619a565bb4
Colin Millar <colin.millar@ices.dk>
2019-10-18 21:02:12 +0200

    fix bootstrap data errors

--- bootstrap/ICES_areas.R
+++ bootstrap/ICES_areas.R
@@ -1,6 +1,6 @@
 library(icesTAF)
 taf.library(icesFO)
 
-areas <- icesFO::load_areas("Baltic Sea")
+areas <- icesFO::load_areas("Barents Sea")
 
 sf::st_write(areas, "areas.csv", layer_options = "GEOMETRY=AS_WKT")
